11a69b78c7cbf8be626143a6340d8c9114ffe90c
Quentin SARAZI <quentin.sarazi@capgemini.com>
2023-01-04 14:04:53 +0100

    toto

--- vector.hpp
+++ vector.hpp
@@ -3,6 +3,8 @@
 
 #include <cstddef>
 
+#include <exception>
+#include <iostream>
 namespace tuto {
 
 template<class T>
@@ -16,20 +18,40 @@ class vector {
 
     vector() = default;
 
+    explicit vector(size_type count) {
+        if (count == 0)
+            return;
+        _data = new value_type[count];
+        _size = count;
+    }
+
+    vector(size_type count, const T& value) {
+        if (count == 0)
+            return;
+        _data = new value_type[count];
+        for (size_type i = 0; i < count; ++i)
+            _data[i] = value;
+        _size = count;
+    }
+
     ~vector() {
-        delete _data;
+        delete[] _data;
+    }
+
+    void assign(size_type count, const T& value) {
+        
     }
 
     reference at(size_type pos) {
         if (pos < _size)
             return _data[pos];
-        throw std::out_of_range;
+        throw std::out_of_range("vector::at out of range");
     }
 
     const_reference at(size_type pos) const {
         if (pos < _size)
             return _data[pos];
-        throw std::out_of_range;
+        throw std::out_of_range("vector::at out of range");
     }
 
     reference operator[](size_type pos) { return _data[pos]; }
@@ -41,12 +63,22 @@ class vector {
     size_type size() const { return _size; }
 
     void push_back(const T& value) {
-        value_type *temp = new value_type[_size + 1];
-        memcpy(temp, _data, _size);
-        std::swap(temp, _data);
-        _data[_size] = value;
-        _size += 1;
-        delete temp;
+        resize(_size + 1, value);
+    }
+
+    void resize(size_type count) {
+        resize(count, value_type{});
+    }
+
+    void resize(size_type count, const value_type& value) {
+        value_type *temp = nullptr;
+        if (count != 0)
+            temp = new value_type[count];
+
+        memcpy(temp, _data, (_size < count ? _size : count));
+        while (_size < count)
+            temp[_size++] = value;
+        std::swap(_data, temp);
     }
 
   private:
